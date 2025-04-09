00c0dd4f3f17b5dc30bf242a51180fb9e2a0ff72
OrGaNiK <OrGaNiK777@yandex.ru>
2025-04-09 16:07:22 +0500

    Prepare for deployment

--- src/App.js
+++ src/App.js
@@ -1,35 +1,37 @@
-import React, { useState } from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Navbar from './components/Navbar';
-import ProductList from './components/ProductList';
-import ProductDetail from './components/ProductDetail';
-import Cart from './components/Cart';
+import React, { useState } from 'react'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import Navbar from './components/Navbar'
+import ProductList from './components/ProductList'
+import ProductDetail from './components/ProductDetail'
+import Cart from './components/Cart'
 
 const App = () => {
-  const [cartItems, setCartItems] = useState([]);
+	const [cartItems, setCartItems] = useState([])
 
-  const addToCart = (item) => {
-    setCartItems([...cartItems, item]);
-  };
+	const addToCart = (item) => {
+		setCartItems([...cartItems, item])
+	}
 
-  const removeFromCart = (index) => {
-    const newCart = [...cartItems];
-    newCart.splice(index, 1);
-    setCartItems(newCart);
-  };
+	const removeFromCart = (index) => {
+		const newCart = [...cartItems]
+		newCart.splice(index, 1)
+		setCartItems(newCart)
+	}
 
-  return (
-    <Router>
-      <div>
-        <Navbar cartCount={cartItems.length} />
-        <Routes>
-          <Route path="/" element={<ProductList />} />
-          <Route path="/product/:id" element={<ProductDetail addToCart={addToCart} />} />
-          <Route path="/cart" element={<Cart cartItems={cartItems} removeFromCart={removeFromCart} />} />
-        </Routes>
-      </div>
-    </Router>
-  );
-};
+	return (
+		<Router>
+			<div>
+				<Navbar cartCount={cartItems.length} />
+				<Routes>
+					<Route basename='/test.a.ya'>
+						<Route path='/' element={<ProductList />} />
+						<Route path='/product/:id' element={<ProductDetail addToCart={addToCart} />} />
+						<Route path='/cart' element={<Cart cartItems={cartItems} removeFromCart={removeFromCart} />} />
+					</Route>
+				</Routes>
+			</div>
+		</Router>
+	)
+}
 
-export default App;
+export default App
